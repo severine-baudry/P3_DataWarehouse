c681a3c64f8ae0c0de5c335e963fc33d9df34db0
severine <simone.yvonne.baudry@gmail.com>
2020-12-06 14:56:04 +0000

    sql queries to create start schema tables, taken from project 1 (not tested)

--- sql_queries.py
+++ sql_queries.py
@@ -43,19 +43,69 @@ CREATE TABLE staging_events_table (
 staging_songs_table_create = ("""
 """)
 
+#songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays
+    (songplay_id SERIAL,
+    start_time bigint references time(timestamp),
+    user_id int references users(user_id),
+    level varchar,
+    song_id varchar references songs(song_id),
+    artist_id varchar references artists(artist_id),
+    session_id int,
+    location varchar(200),
+    useragent varchar(200),
+    PRIMARY KEY(user_id, song_id, start_time)
+    );
 """)
 
+#user_id, first_name, last_name, gender, level
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users
+    ( user_id int PRIMARY KEY,
+    first_name varchar,
+    last_name varchar,
+    gender char,
+    level varchar
+    );
 """)
 
+#song_id, title, artist_id, year, duration
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs
+    (
+    song_id varchar PRIMARY KEY,
+    title varchar(200),
+    artist_id varchar references artists(artist_id),
+    year int,
+    duration real
+    );
 """)
 
+#artist_id, name, location, lattitude, longitude
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists
+    (
+    artist_id varchar PRIMARY KEY,
+    name varchar,
+    location varchar,
+    latitude real,
+    longitude real
+    );
 """)
 
+#start_time, hour, day, week, month, year, weekday
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time
+    (
+    start_time bigint PRIMARY KEY,
+    hour int,
+    day int,
+    weekofyear int,
+    month int,
+    year int,
+    dayofweek int
+    );
 """)
 
 # STAGING TABLES
